a345afe7dc8dd3a0d43dcb96735a6549b4236cae
D3kion <dekion.dev@gmail.com>
2021-05-31 21:46:33 +0300

    added header logo to home page

--- src/core/home/home.page.js
+++ src/core/home/home.page.js
@@ -2,6 +2,7 @@ import { IndentLayout } from '../../lib/element/layout';
 
 import { FooterContainer } from '../footer';
 import { HeaderContainer } from '../header';
+import { HeaderLogoContainer } from '../header-logo';
 
 import { HomeContainer } from './home.container';
 
@@ -9,7 +10,10 @@ export function HomePage() {
   return (
     <IndentLayout type="large">
       <IndentLayout type="medium">
-        <HeaderContainer />
+        <IndentLayout type="small">
+          <HeaderLogoContainer />
+          <HeaderContainer />
+        </IndentLayout>
         <HomeContainer />
       </IndentLayout>
       <FooterContainer />
