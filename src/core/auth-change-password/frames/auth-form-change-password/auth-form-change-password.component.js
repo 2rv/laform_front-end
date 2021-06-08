7ad82e632b35b5a51a86652cf0b85ce85d9dc3a0
D3kion <dekion.dev@gmail.com>
2021-06-08 17:02:03 +0300

    code review fixes

--- src/core/auth-change-password/frames/auth-form-change-password/auth-form-change-password.component.js
+++ src/core/auth-change-password/frames/auth-form-change-password/auth-form-change-password.component.js
@@ -1,44 +1,27 @@
-import styled from 'styled-components';
-
-import { spacing, THEME_SIZE } from '../../../../lib/theme';
 import { FieldPrimary } from '../../../../lib/element/field';
-import { TextPrimary } from '../../../../lib/element/text';
+import { TitlePrimary } from '../../../../lib/element/title';
 import { ButtonPrimary } from '../../../../lib/element/button';
+import { FieldLayout, IndentLayout } from '../../../../lib/element/layout';
 
 export function AuthFormChangePasswordComponent(props) {
   return (
-    <Container>
-      <Title tid="AUTH_FORM_CHANGE_PASSWORD.TITLE" />
-      <FieldContainer>
+    <IndentLayout type="small">
+      <TitlePrimary tid="AUTH_FORM_CHANGE_PASSWORD.TITLE" />
+      <FieldLayout>
         <FieldPrimary
-          titleTid="AUTH_FORM_CHANGE_PASSWORD.OLD_PASSWORD"
-          placeholderTid="AUTH_FORM_CHANGE_PASSWORD.P_OLD_PASSWORD"
+          titleTid="AUTH_FORM_CHANGE_PASSWORD.OLD_PASSWORD.TITLE"
+          placeholderTid="AUTH_FORM_CHANGE_PASSWORD.OLD_PASSWORD.PLACEHOLDER"
         />
         <FieldPrimary
-          titleTid="AUTH_FORM_CHANGE_PASSWORD.NEW_PASSWORD"
-          placeholderTid="AUTH_FORM_CHANGE_PASSWORD.P_NEW_PASSWORD"
+          titleTid="AUTH_FORM_CHANGE_PASSWORD.NEW_PASSWORD.TITLE"
+          placeholderTid="AUTH_FORM_CHANGE_PASSWORD.NEW_PASSWORD.PLACEHOLDER"
         />
         <FieldPrimary
-          titleTid="AUTH_FORM_CHANGE_PASSWORD.REPEAT_NEW_PASSWORD"
-          placeholderTid="AUTH_FORM_CHANGE_PASSWORD.P_REPEAT_NEW_PASSWORD"
+          titleTid="AUTH_FORM_CHANGE_PASSWORD.REPEAT_NEW_PASSWORD.TITLE"
+          placeholderTid="AUTH_FORM_CHANGE_PASSWORD.REPEAT_NEW_PASSWORD.PLACEHOLDER"
         />
-      </FieldContainer>
+      </FieldLayout>
       <ButtonPrimary tid="AUTH_FORM_CHANGE_PASSWORD.SUBMIT" />
-    </Container>
+    </IndentLayout>
   );
 }
-
-const Container = styled.div`
-  display: grid;
-  gap: ${spacing(3)};
-`;
-
-const Title = styled(TextPrimary)`
-  font-size: ${THEME_SIZE.FONT.LARGE};
-  font-weight: ${THEME_SIZE.FONT_WEIGHT.MEDIUM};
-`;
-
-const FieldContainer = styled.div`
-  display: grid;
-  gap: ${spacing(2)};
-`;
