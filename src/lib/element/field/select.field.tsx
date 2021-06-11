546a15a6a912932db248e7f2d7ca15496cefea65
D3kion <dekion.dev@gmail.com>
2021-06-11 20:47:59 +0300

    code review fix

--- src/lib/element/field/select.field.tsx
+++ src/lib/element/field/select.field.tsx
@@ -1,5 +1,7 @@
 import styled from 'styled-components';
 
+import { ReactComponent as ArrowDown } from '../../../asset/svg/arrow-down-solid.svg';
+
 import { text } from '../../common/text';
 import { spacing, THEME_COLOR, THEME_SIZE, THEME_VALUE } from '../../theme';
 import { TextSecondary } from '../text';
@@ -13,13 +15,16 @@ export function FieldSelect(props: SelectPropsType) {
   return (
     <IndentLayout type="text_small">
       {titleTid && <Title tid={titleTid} />}
-      <Select name={name} value={value} onChange={onChange}>
-        {options.map(({ id, tid, tvalue }) => (
-          <option key={id} value={id}>
-            {text(tid, tvalue)}
-          </option>
-        ))}
-      </Select>
+      <InputContainer>
+        <Select name={name} value={value} onChange={onChange}>
+          {options.map(({ id, tid, tvalue }) => (
+            <option key={id} value={id}>
+              {text(tid, tvalue)}
+            </option>
+          ))}
+        </Select>
+        <ArrowDownIcon />
+      </InputContainer>
     </IndentLayout>
   );
 }
@@ -28,7 +33,14 @@ const Title = styled(TextSecondary)`
   font-size: ${THEME_SIZE.FONT.SMALL};
 `;
 
+const InputContainer = styled.div`
+  position: relative;
+  display: flex;
+  align-items: center;
+`;
+
 const Select = styled.select`
+  width: 100%;
   padding: ${spacing(3)};
   background: ${THEME_COLOR.BACKGROUND.GRAY};
   border: 0;
@@ -36,10 +48,10 @@ const Select = styled.select`
   font-family: ${THEME_VALUE.FONT_NAME.PRIMARY};
   font-size: ${THEME_SIZE.FONT.SMALL};
   color: ${THEME_COLOR.SECONDARY_DARK};
-
   appearance: none;
-  background-image: url("data:image/svg+xml,%3Csvg width='8' height='7' viewBox='0 0 8 7' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M4.86603 6.5C4.48113 7.16667 3.51887 7.16667 3.13397 6.5L0.535899 2C0.150999 1.33333 0.632124 0.499999 1.40192 0.499999L6.59808 0.5C7.36788 0.5 7.849 1.33333 7.4641 2L4.86603 6.5Z' fill='%23808080'/%3E%3C/svg%3E%0A");
-  background-repeat: no-repeat;
-  background-position-x: 98%;
-  background-position-y: 21px;
+`;
+
+const ArrowDownIcon = styled(ArrowDown)`
+  position: absolute;
+  right: ${spacing(3)};
 `;
