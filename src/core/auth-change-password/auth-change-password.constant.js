bcc45246aa75b046442051d781be116710c44791
D3kion <dekion.dev@gmail.com>
2021-06-14 20:53:49 +0300

    adjusted routes according to backend changes

--- src/core/auth-change-password/auth-change-password.constant.js
+++ src/core/auth-change-password/auth-change-password.constant.js
@@ -6,7 +6,7 @@ export const AUTH_CHANGE_PASSWORD_ROUTE_PATH = '/auth/change-password';
 export const AUTH_CHANGE_PASSWORD_STORE_NAME = 'AUTH_CHANGE_PASSWORD';
 
 export const AUTH_CHANGE_PASSWORD_API = {
-  ENDPOINT: '/user-recovery/change-password',
+  ENDPOINT: '/user-recovery/change-credentials',
   METHOD: 'POST',
 };
 
