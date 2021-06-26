2f736998adc43e5d548674a95e8c9d7ff36d3eef
Pasha1neo <pzx5@bk.ru>
2021-06-26 16:08:54 +0300

    home-banner-done

---
+++ src/core/home/frames/banner/banner.constant.js
@@ -0,0 +1,20 @@
+export const BANNER_LIST_ITEMS = [
+  {
+    title: 'HOME.BANNER.BANNER_1.TITLE',
+    backgroundImage: '/static/image/slider-item-1.png',
+    buttonText: 'OTHER.BUY',
+    path: '/',
+  },
+  {
+    title: 'HOME.BANNER.BANNER_2.TITLE',
+    backgroundImage: '/static/image/slider-item-1.png',
+    buttonText: 'OTHER.BUY',
+    path: '/',
+  },
+  {
+    title: 'HOME.BANNER.BANNER_3.TITLE',
+    backgroundImage: '/static/image/slider-item-1.png',
+    buttonText: 'OTHER.BUY',
+    path: '/',
+  },
+];
