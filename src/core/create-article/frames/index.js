327fc9a5b2754133bbf3445082fe52fc52a71554
Pasha1neo <pzx5@bk.ru>
2021-08-16 13:00:47 +0300

    fix

--- src/core/create-article/frames/index.js
+++ src/core/create-article/frames/index.js
@@ -1,3 +1,2 @@
 export { CreateArticleImageComponent } from './create-article-image';
-export { CreateArticleFormContainer } from './create-article-form';
 export { CreateArticleFieldComponent } from './create-article-field';
