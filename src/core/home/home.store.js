f6677053eea369579d37baf132850eb838a9f616
Pasha1neo <pzx5@bk.ru>
2021-08-24 13:43:11 +0300

    get-home-data-is-done

--- src/core/home/home.store.js
+++ src/core/home/home.store.js
@@ -4,35 +4,72 @@ import {
   setRequestPending,
   setRequestSuccess,
 } from '../../main/store/store.service';
-
 import { HOME_ACTION_TYPE } from './home.type';
 
 const initialState = {
-  pinnedMasterClasses: initRequestState(),
+  masterClassState: initRequestState(),
+  sewingGoodsState: initRequestState(),
+  articleState: initRequestState(),
 };
 
 export function homeStore(state = initialState, action) {
   switch (action.type) {
-    case HOME_ACTION_TYPE.HOME_UPLOAD_PENDING:
+    case HOME_ACTION_TYPE.HOME_MASTER_CLASS_PENDING:
       return {
         ...state,
-        pinnedMasterClasses: setRequestPending(state.pinnedMasterClasses),
+        masterClassState: setRequestPending(state.masterClassState),
       };
-    case HOME_ACTION_TYPE.HOME_UPLOAD_SUCCESS:
+    case HOME_ACTION_TYPE.HOME_MASTER_CLASS_SUCCESS:
       return {
         ...state,
-        pinnedMasterClasses: setRequestSuccess(
-          state.pinnedMasterClasses,
-          action.pinnedMasterClasses,
+        masterClassState: setRequestSuccess(
+          state.masterClassState,
+          action.data,
         ),
       };
-    case HOME_ACTION_TYPE.HOME_UPLOAD_ERROR:
+    case HOME_ACTION_TYPE.HOME_MASTER_CLASS_ERROR:
       return {
         ...state,
-        pinnedMasterClasses: setRequestError(
-          state.pinnedMasterClasses,
+        masterClassState: setRequestError(
+          state.masterClassState,
           action.errorMessage,
         ),
+      };
+    case HOME_ACTION_TYPE.HOME_SEWING_GOODS_PENDING:
+      return {
+        ...state,
+        sewingGoodsState: setRequestPending(state.sewingGoodsState),
+      };
+    case HOME_ACTION_TYPE.HOME_SEWING_GOODS_SUCCESS:
+      return {
+        ...state,
+        sewingGoodsState: setRequestSuccess(
+          state.sewingGoodsState,
+          action.data,
+        ),
+      };
+    case HOME_ACTION_TYPE.HOME_SEWING_GOODS_ERROR:
+      return {
+        ...state,
+        sewingGoodsState: setRequestError(
+          state.sewingGoodsState,
+          action.errorMessage,
+        ),
+      };
+    case HOME_ACTION_TYPE.HOME_ARTICLE_PENDING:
+      return {
+        ...state,
+        articleState: setRequestPending(state.articleState),
+      };
+    case HOME_ACTION_TYPE.HOME_ARTICLE_SUCCESS:
+      return {
+        ...state,
+        articleState: setRequestSuccess(state.articleState, action.data),
+      };
+    case HOME_ACTION_TYPE.HOME_ARTICLE_ERROR:
+      return {
+        ...state,
+        articleState: setRequestError(state.articleState, action.errorMessage),
       };
     default:
       return state;
