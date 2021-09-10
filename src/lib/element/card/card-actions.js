f44f3b063258862c10747e6c34b4c21e7f9c1c46
TAbrahamyan <t.abrahamyann@gmail.com>
2021-09-10 11:57:56 +0400

    Minor changes

--- src/lib/element/card/card-actions.js
+++ src/lib/element/card/card-actions.js
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
 import { spacing, THEME_SIZE, THEME_COLOR } from '../../theme';
 import { ButtonSecondary, ButtonPrimary, IconButton } from '../button';
-import { ModalPopup } from '../modal';
+import { Popup } from '../popup';
 import { ReactComponent as LikeIcon } from '../../../asset/svg/favorite-icon.svg';
 import { ReactComponent as Delete } from '../../../asset/svg/delete-cancel-icon.svg';
 import { useEffect, useState } from 'react';
@@ -21,7 +21,6 @@ export function CardActions(props) {
   const [isLiked, setLike] = useState(like); // Стейт лайка для лайка
   const [inCart, setInCart] = useState(cart); // Стейт корзины для покупки
   const [isSelected, setSelect] = useState(selected); // Стейт выбрано или не выбрано для выбора товара в рекомендации
-  const [modalVisibilty, setModalVisibility] = useState(selected); // Стейт выбрано или не выбрано для выбора товара в рекомендации
   const [selectText, setSelectText] = useState(
     purchase ? 'OTHER.PURCHASED' : null,
   ); // Стейт текста который должен быть на кнопке
@@ -47,15 +46,6 @@ export function CardActions(props) {
     }
   };
 
-  const closeModal = () => {
-    setModalVisibility(false);
-  };
-
-  const deleteProduct = () => {
-    onDeleteProduct(id, { deleted: true });
-    closeModal();
-  };
-
   return (
     <LineCase>
       <Button
@@ -69,20 +59,28 @@ export function CardActions(props) {
         <LikeIcon />
       </LikeButton>
       {isAdmin && (
-        <>
-          <IconButton onClick={() => setModalVisibility(true)}>
-            <Delete />
-          </IconButton>
-          <ModalPopup modalVisibilty={modalVisibilty} onClose={closeModal}>
+        <Popup
+          content={(setVisible) => (
             <ModalContent>
               <TextSecondary tid="Вы точно хотите удалить данный товар? Это действие нельзя отменить" />
               <ModalButtons>
-                <ButtonSecondary tid="Да" onClick={deleteProduct} />
-                <ButtonPrimary tid="Отмена" onClick={closeModal} />
+                <ButtonSecondary
+                  tid="Да"
+                  onClick={() => {
+                    onDeleteProduct(id, { deleted: true });
+                    setVisible(false);
+                  }}
+                />
+                <ButtonPrimary tid="Отмена" onClick={() => setVisible(false)} />
               </ModalButtons>
             </ModalContent>
-          </ModalPopup>
-        </>
+          )}
+          children={(
+            <IconButton>
+              <Delete />
+            </IconButton>
+          )}
+        />
       )}
     </LineCase>
   );
@@ -103,13 +101,15 @@ const LikeButton = styled(IconButton)`
   background-color: ${(p) =>
     p.like ? THEME_COLOR.DARK_GRAY : THEME_COLOR.GRAY};
 `;
+
 const ModalContent = styled.div`
   display: grid;
-  gap: ${spacing(3)};
-  width: 100%;
+  width: 340px;
+  gap: ${spacing(2)};
+  padding: ${spacing(2)};
 `;
 
 const ModalButtons = styled.div`
   display: flex;
-  gap: ${spacing(3)};
+  gap: ${spacing(2)};
 `;
