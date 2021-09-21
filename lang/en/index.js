fb86e767e63c6eeeb0da01d449d1742e67dd6b0e
TAbrahamyan <t.abrahamyann@gmail.com>
2021-09-21 07:57:27 +0400

    Added english localization

--- lang/en/index.js
+++ lang/en/index.js
@@ -12,7 +12,31 @@ import INFORMATION_DIRECTORY_LIST from './information-directory-list.json';
 import FAQ_LIST from './faq-list.json';
 import AUTH from './auth.json';
 import SETTINGS from './settings.json';
+import OTHER from './other.json';
+import PATTERNS from './patterns.json';
+import ARTICLES from './articles.json';
+import SEWING_GOODS from './sewing-goods.json';
+import MASTER_CLASSES from './master-classes.json';
 import ERROR_PAGE from './error-page.json';
+import BASKET from './basket.json';
+import PURCHASE from './purchase.json';
+import COMPILATION from './compilation.json';
+import ORDERS from './orders.json';
+import ORDER_NUMBER from './order-number.json';
+import NOTIFICATION from './notification.json';
+import UNIT from './unit.json';
+import PROFILE from './profile.json';
+import ARTICLE_CREATE_FORM from './article-create-form.json';
+import COMMENTS from './comments.json';
+import CREATE_NOTIFICATION from './create-notification.json';
+import SLIDER from './slider.json';
+import PRODUCT_IMAGES from './product-images.json';
+import PRODUCT_PRICE from './product-price.json';
+import DYNAMIC_FIELDS from './dynamic-fields.json';
+import FAVORITES from './favorites.json';
+import BLOCK_TEXT from './block-text.json';
+import BLOCK_TABLE_LIST from './block-table-list.json';
+import PROMOCODE from './promocode.json';
 
 export const EN = {
   translation: {
@@ -30,6 +54,30 @@ export const EN = {
     FAQ_LIST,
     AUTH,
     SETTINGS,
+    OTHER,
+    PATTERNS,
+    ARTICLES,
+    SEWING_GOODS,
+    MASTER_CLASSES,
     ERROR_PAGE,
+    BASKET,
+    PURCHASE,
+    COMPILATION,
+    ORDER_NUMBER,
+    ORDERS,
+    NOTIFICATION,
+    UNIT,
+    PROFILE,
+    ARTICLE_CREATE_FORM,
+    CREATE_NOTIFICATION,
+    SLIDER,
+    PRODUCT_IMAGES,
+    PRODUCT_PRICE,
+    DYNAMIC_FIELDS,
+    FAVORITES,
+    COMMENTS,
+    BLOCK_TEXT,
+    BLOCK_TABLE_LIST,
+    PROMOCODE,
   },
 };
