07e5a9419e806df6877ba6b5118c0555889cbdbd
TAbrahamyan <t.abrahamyann@gmail.com>
2021-09-24 14:25:02 +0400

    Added purchased master-class page data

--- src/core/master-class-page/master-class-page.container.js
+++ src/core/master-class-page/master-class-page.container.js
@@ -1,6 +1,5 @@
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { LANG_STORE_NAME } from 'src/lib/common/lang';
 import { getQuery } from 'src/main/navigation';
 import { NAVIGATION_STORE_NAME } from '../../lib/common/navigation';
 import {
@@ -10,21 +9,20 @@ import {
   isRequestPending,
   isRequestSuccess,
 } from '../../main/store/store.service';
-import { masterClassPageUploadData } from './master-class-page.action';
+import { purchasedMasterClassUploadData } from './master-class-page.action';
 import { MasterClassPageComponent } from './master-class-page.component';
 import { MASTER_CLASS_PAGE_STORE_NAME } from './master-class-page.constant';
 
 export function MasterClassPageContainer() {
   const dispatch = useDispatch();
   const purchaseId = getQuery('id');
-  const { state, pageLoading, currentLang } = useSelector((state) => ({
+  const { state, pageLoading } = useSelector((state) => ({
     state: state[MASTER_CLASS_PAGE_STORE_NAME].masterClass,
     pageLoading: state[NAVIGATION_STORE_NAME].pageLoading,
-    currentLang: state[LANG_STORE_NAME].active.toLowerCase(),
   }));
 
   useEffect(() => {
-    dispatch(purchasedMasterClassUploadData(currentLang, purchaseId));
+    dispatch(purchasedMasterClassUploadData(purchaseId));
   }, []);
 
   return (
--- src/core/master-class-page/master-class-page.convert.js
+++ src/core/master-class-page/master-class-page.convert.js
@@ -1,9 +1,9 @@
 export function convertPurchasedMasterClassData(rowData) {
   return {
     id: rowData.id,
-    type: rowData?.type,
-    name: rowData?.titleRu,
-    articleText: rowData?.articleText,
     createdDate: rowData?.createdDate,
+    name: rowData?.masterClassId?.titleRu,
+    type: rowData?.masterClassId?.type,
+    articleText: rowData?.program?.articleText,
   };
 }
