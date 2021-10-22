f2db6e3cac9465af647c194b1c3211106a2f6cae
pasha1neo <pasha1neo@mail.ru>
2021-10-22 13:36:53 +0300

    fix

--- src/core/patterns-page/patterns-page.action.js
+++ src/core/patterns-page/patterns-page.action.js
@@ -20,6 +20,7 @@ export function patternsPageUploadData(id) {
         payload: convertPatternData(response.data),
       });
     } catch (err) {
+      console.log(err);
       if (err.response) {
         dispatch({
           type: PATTERNS_PAGE_ACTION_TYPE.PATTERNS_PAGE_UPLOAD_ERROR,
