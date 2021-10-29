f4e7d863016009d09d172f1411b5637fccf15bbb
TAbrahamyan <t.abrahamyann@gmail.com>
2021-10-29 19:46:39 +0400

    Minor change

--- src/core/signup/signup.convert.js
+++ src/core/signup/signup.convert.js
@@ -1,7 +1,7 @@
 import { SIGNUP_DATA_NAME, SIGNUP_FIELD_NAME } from './signup.type';
 
 export const convertSignupFormData = (data) => ({
-  [SIGNUP_DATA_NAME.LOGIN]: data[SIGNUP_FIELD_NAME.LOGIN].trim(),
+  [SIGNUP_DATA_NAME.LOGIN]: data[SIGNUP_FIELD_NAME.LOGIN].trim().toLowerCase(),
   [SIGNUP_DATA_NAME.EMAIL]: data[SIGNUP_FIELD_NAME.EMAIL].trim(),
   [SIGNUP_DATA_NAME.PASSWORD]: data[SIGNUP_FIELD_NAME.PASSWORD],
 });
