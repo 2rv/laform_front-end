e2781593615337ac45fae92307919536c464593d
pasha1neo <pasha1neo@mail.ru>
2021-12-15 16:12:53 +0300

    change

--- src/core/master-class-page/master-class-page.convert.js
+++ src/core/master-class-page/master-class-page.convert.js
@@ -1,10 +1,12 @@
 export function convertPurchasedMasterClassData(rowData) {
+  console.log(rowData);
   return {
     id: rowData.id,
     createdDate: rowData?.createdDate,
     name: rowData?.masterClassId?.titleRu || rowData.titleEn,
     type: rowData?.masterClassId?.type,
-    materials: rowData?.masterClassId?.materialRu || rowData?.masterClassId?.materialEn,
+    materials:
+      rowData?.masterClassId?.materialRu || rowData?.masterClassId?.materialEn,
     articleText:
       rowData?.masterClassId?.articleRu || rowData?.masterClassId?.articleEn,
   };
